1a64adb700b4c4966b9e6049916a3eb5692bb169
Ravi Teja Rajavarapu <50618333+iamraviteja18@users.noreply.github.com>
2020-12-31 19:06:01 +0530

    Update freshworksassignment.py

--- freshworksassignment.py
+++ freshworksassignment.py
@@ -36,7 +36,7 @@ class freshworks:
                     s=str(key)+":"+str(k[0]) # returning the key name,value in JasonObject format,"keyname:val"
                     print(s)
                 else:
-                    del dic[key]
+                    del dic[key] #deleting the key, val from the store as tome-to-live got expired
                     print("Error: Key no longer exists as time-to-live of",key,"has expired") #error when time to live of the given key gets expired
 
  
